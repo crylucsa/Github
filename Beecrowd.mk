88cfcacf3ede3ce70c3efb3bc3ccc8c3f25f79e0
crylucsa <lucsacosta391@gmail.com>
2025-08-28 08:55:53 -0300

    AtividadesBeecrowd(SQL)

---
+++ Beecrowd.mk
@@ -0,0 +1,34 @@
+# Problems SQL
+
+## Atividade 6203
+![Atividade 6203](./04109098-160c-404e-b6f8-a2d68e90df81.png)
+
+## Atividade 6204
+![Atividade 6204](./741096db-98b5-4885-bf66-5a9ab71eb2e4.png)
+
+## Atividade 6206
+![Atividade 6206](./2dbbf11c-f9b5-48a1-ad12-ce510949f23b.png)
+
+## Atividade 6207
+![Atividade 6207](./56a0b86e-39d1-4c60-a5d6-99b4746f61c4.png)
+
+## Atividade 6208
+![Atividade 6208](./feff818a-3d31-447f-aa65-59815bc0f694.png)
+
+## Atividade 6610
+![Atividade 6610](./e6d6df90-f56b-44a7-956a-4dfa85676d9b.png)
+
+## Atividade 6213
+![Atividade 6213](./6d04754d-9858-4c21-80c0-f380f8942ff7.png)
+
+## Atividade 6215
+![Atividade 6215](./c937d0ad-0c16-4646-a570-2df51e674acf.png)
+
+## Atividade 6218
+![Atividade 6218](./ae7cf677-a857-49d8-b035-364d7903a1c7.png)
+
+## Atividade 2622
+![Atividade 2622](./28f820ae-f7b7-4a00-b1a4-8a7decf6f236.png)
+
+## Fontes de pesquisa
+- [W3Schools - SQL LEFT JOIN](https://www.w3schools.com/sql/sql_join_left.asp)
